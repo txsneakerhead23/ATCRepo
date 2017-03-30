5d2c6876fe1805b6f80b3adb6e4aa0164843b437
owenkealey <kealeyo19@students.spprep.org>
2017-03-30 08:53:11 -0400

    Update config.py

--- supreme/config.py
+++ supreme/config.py
@@ -1,3 +1,4 @@
 path = "/path/to/your/chrome/profile"
 key = 'captchasolutions key' #paste the key inside the single quotes; do the same for the secret
 secret = 'captchasolutionssecret'
+operatingsystem = 'Windows' #replace Windows with LInux or Mac if you are using either of those OSs
