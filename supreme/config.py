e9de0a940858e8e265d45a8de682a5ba374bee7c
owenkealey <kealeyo19@students.spprep.org>
2017-04-01 21:36:31 -0400

    Delete config.py

--- supreme/config.py
+++
@@ -1,4 +0,0 @@
-path = "/path/to/your/chrome/profile"
-key = 'captchasolutions key' #paste the key inside the single quotes; do the same for the secret
-secret = 'captchasolutionssecret'
-operatingsystem = 'Windows' #replace Windows with Linux or Mac if you are using either of those OSs
