106b641a4359dbb884b51e67cec7c866058cf7ee
owenkealey <kealeyo19@students.spprep.org>
2017-04-09 01:29:18 -0400

    Add files via upload

---
+++ supreme/config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+path = "/path/to/your/chrome/profile"
+
+key = "key"
+
+secret = "secret"
+
+
+name = "John Doe"
+
+email = "grandstreetsupreme@gmail.com"
+
+firsttel = "000"
+
+midtel = "000"
+
+lasttel = "0000"
+
+address = "4 Hypebeast Plaza"
+
+zipe = "11111"
+
+ccnumber = "1111111111111111"
+
+cvv = "000"
